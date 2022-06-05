4faa878b5d776c19513f919f2778ef1a5246f6dd
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-06-04 22:17:14 -0600

    Initial Commit

---
+++ Shared/RickAndMortyApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApiApp.swift
+//  Shared
+//
+//  Created by Jesús Francisco Leyva Juárez on 04/06/22.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
