bd9cdb06ea9f8d61c04e7a78fdd8733a7fda5d66
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-06-04 23:22:10 -0600

    view with image

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var vm = viewModel()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(vm.datos.results, id: \.id) { personaje in
+                HStack {
+                    let urlImage = URL(string: personaje.image)
+                    AsyncImage(url: urlImage) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    }
+                placeholder: {
+                    Color.purple.opacity(0.1)
+                }
+                .frame (width: 80, height: 80)
+                .cornerRadius(20)
+                    
+                    VStack(alignment: .leading) {
+                        Text(personaje.name)
+                            .font(.headline)
+                            .foregroundColor(.black)
+                        Text(personaje.type)
+                            .font(.body)
+                    }
+                }.padding()
+            }
+            .navigationTitle("Rick & Morty")
+        }
+        .task{
+            await vm.fetchData()
+        }
     }
 }
 
